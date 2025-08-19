7c9051d023a6e24a1e754da8111614dfa3a5eca3
Nigel James <nigel@james.gen.nz>
2025-08-19 20:05:35 +1200

    Backup triggered on 2025-08-19 20:05:35
     "klipper on commit: v0.13.0-60879fd"
    
     "moonraker on commit: v0.9.3-f20feff"
    
     "fluidd on commit: v1.34.3"
    
     "KlipperScreen on commit: v0.4.6-b05aadc"
    
     "crowsnest on commit: v4.1.16-059fa87"

--- scripts/mygitbackup.sh
+++ scripts/mygitbackup.sh
@@ -32,9 +32,7 @@ for repo in "${gitrepos[@]}"; do
       if [ -e ${repo}/.version ]; then
          rev="${trimedRepo} on commit: $(head -n 1 ${repo}/.version)"
       else
-         #rev="${trimedRepo} on commit: $(git -C ${repo} tag -l | tail -1)-$(git -C ${repo} rev-parse --short=7 HEAD)"
-         rev="${trimedRepo} on commit: $(git -C ${repo} tag --sort=-version:refname | head -n 1)"
-         #echo $rev
+         rev="${trimedRepo} on commit: $(git -C ${repo} tag -l --sort=v:refname | tail -1)-$(git -C ${repo} rev-parse --short=7 HEAD)"
       fi
       gitrevs+=("-m \"${rev}\"")
       echo $rev
